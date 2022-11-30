16d8a67de30d23986248a5ec3a81a7a7fc946b9d
Lucas de Wolff <33374947+lcw@users.noreply.github.com>
2022-11-30 11:58:38 +0100

    Update Data_visualisation.py

--- Data_visualisation.py
+++ Data_visualisation.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 
 # TODO: 
-path = 'data\Boundaries - Neighborhoods\geo_export_7ad36bff-e877-4687-85bf-007c957c1dd9.shp'
+path = 'data\shapefile\geo_export.shp'
 africa = gpd.read_file(path)
 # africa = africa[africa['ISO3'].notna()]
 africa.plot()
-plt.title("Africa 2020")
+plt.title("Chicago")
 plt.xticks([])
 plt.yticks([])
 plt.show()
